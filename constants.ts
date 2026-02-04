80e089257f4cb61f2b9a0a5975dbcecf29b5dee2
Gheren <saputragheren@gmail.com>
2026-02-05 00:09:17 +0700

    Updated TS Data Part 2

--- constants.ts
+++ constants.ts
@@ -1,81 +1,336 @@
-
 import { Mentor } from './types';
 
 export const MOCK_MENTORS: Mentor[] = [
-  { 
-    name: "Septiyan Dwi Putro Wibowo", 
-    university: "UI", 
-    major: "FISIKA", 
-    path: "SNBP", 
-    category: "PTN",
-    achievements: ["Juara 2 OSN Fisika Kota", "Ketua Kir"]
-  },
-  { 
-    name: "Rahma Arifa Wulandari", 
-    university: "ITB", 
-    major: "Teknik Geodesi", 
-    path: "SNBT",
-    category: "PTN",
-    achievements: ["Lolos Mandiri UNPAD", "Lolos Mandiri ITS"]
-  },
-  { 
-    name: "Gheren Ramandra Saputra", 
-    university: "IPB", 
-    major: "Ilmu Komputer", 
+  {
+    name: "Siti Kirani Nurassifa Aminah",
+    university: "UPN Veteran Jakarta",
+    major: "Biologi",
     path: "Mandiri",
     category: "PTN",
-    achievements: ["Ketua OSIS 2023/2024", "Lolos SNBT Udayana", "Gold Medal LKS"]
+    achievements: ["Penmaba UNJ: Lulus D4 Pemasaran Digital", "Penmaru UPNVJ: Lulus S1 Biologi"]
   },
-  { 
-    name: "Axel Kenzie Brata", 
-    university: "BINUS", 
-    major: "Cyber Security", 
-    path: "Beasiswa",
-    category: "PTS",
-    achievements: ["Full Scholarship Binus", "Bug Bounty Hunter"]
-  },
-  { 
-    name: "Wildan", 
-    university: "Monash University", 
-    major: "Business Data Analytics", 
-    path: "Beasiswa Luar Negeri",
-    category: "PTLN",
-    achievements: ["Lolos Beasiswa Indonesia Maju (BIM)", "Lolos SNBT Unair", "IELTS 7.5"]
-  },
-  { 
-    name: "Rafha Aulia", 
-    university: "UIN Jakarta", 
-    major: "Psikologi", 
-    path: "Reguler",
+  {
+    name: "Zhalisha Athaya Fatihah",
+    university: "Universitas Indonesia",
+    major: "Manajemen Rekod dan Arsip",
+    path: "Mandiri PPKB UI",
     category: "PTN",
-    achievements: ["Lolos Mandiri UNJ"]
+    achievements: ["PPKB UI (Manajemen Rekod dan Arsip)", "Jalur Prestasi Vokasi UNDIP"]
   },
-  { 
-    name: "Aditya Pratama", 
-    university: "UGM", 
-    major: "Hukum", 
-    path: "SNBT",
+  {
+    name: "Gheren Ramandra Saputra",
+    university: "Institut Pertanian Bogor",
+    major: "Ilmu Komputer",
+    path: "Mandiri (Jalur Ketua OSIS)",
     category: "PTN",
-    achievements: ["Lolos SIMAK UI (Vokasi)", "Debater Terbaik Sekolah"]
+    achievements: ["Lulus UNJ (Ilmu Komputer)", "Lulus IPB University (Ilmu Komputer)"]
   },
-  { 
-    name: "Siti Nurhaliza", 
-    university: "UNPAD", 
-    major: "Komunikasi", 
+  {
+    name: "Chiesa Abby Putra",
+    university: "Universitas Jenderal Soedirman",
+    major: "Bisnis Internasional",
+    path: "Mandiri",
+    category: "PTN",
+    achievements: ["Undip & UPNVJ Teknik Industri", "Unsoed Teknik Pertanian & Bisnis Internasional"]
+  },
+  {
+    name: "Septiyan Dwi Putra Wibowo",
+    university: "Universitas Indonesia",
+    major: "Fisika",
     path: "SNBP",
     category: "PTN",
-    achievements: ["Host Podcast Sekolah"]
+    achievements: ["Lulus jalur SNBP di UI - Fisika"]
   },
-  { 
-    name: "Budi Santoso", 
-    university: "ITS", 
-    major: "Teknik Mesin", 
+  {
+    name: "Kevin Delprian",
+    university: "Politeknik Negeri Jakarta",
+    major: "Manajemen Keuangan",
+    path: "SNBT",
+    category: "PTN",
+    achievements: ["Juara 2 Basket HT Cup 2022", "Komisi B MPK 2024", "Bendahara Umum Gathering Prodi 2025"]
+  },
+  {
+    name: "Muhammad Putra Sofyan",
+    university: "Universitas Tarumanagara",
+    major: "Psikologi",
+    path: "Reguler Tanpa TES",
+    category: "PTS",
+    achievements: []
+  },
+  {
+    name: "Mirza Firmansyah",
+    university: "Perbanas Institute",
+    major: "S1 Sistem Informasi",
+    path: "Reguler Tanpa TES",
+    category: "PTS",
+    achievements: ["Mantan Kadiv Venue Manager HangTuahCup 2025"]
+  },
+  {
+    name: "Irgi Dwi Saputra",
+    university: "Universitas Diponegoro",
+    major: "Teknologi Hasil Perikanan",
     path: "Mandiri",
     category: "PTN",
-    achievements: ["Lolos UTUL UGM", "Lolos SMUP Unpad"]
+    achievements: ["Lulus jalur Mandiri di Undip - Teknologi Hasil Perikanan"]
   },
+  {
+    name: "Gilvara Delia Sayidina",
+    university: "Universitas Padjadjaran",
+    major: "Jurnalistik",
+    path: "SNBT",
+    category: "PTN",
+    achievements: ["Wakil Ketua Osis"]
+  },
+  {
+    name: "Kyla Zafirah",
+    university: "UIN Syarif Hidayatullah Jakarta",
+    major: "Kesejahteraan Sosial",
+    path: "SNBP",
+    category: "PTN",
+    achievements: ["Lulus jalur SNBP di UINJKT - Kesejahteraan Sosial"]
+  },
+  {
+    name: "Daffa Anargya Maheswara",
+    university: "UIN Syarif Hidayatullah Jakarta",
+    major: "Sistem Informasi",
+    path: "SNBT",
+    category: "PTN",
+    achievements: ["Bendahara MPK", "Sekretaris 2 Hang Tuah Cup 2025", "Ketua Pelaksana Hari Guru 2024"]
+  },
+  {
+    name: "Rafha Aulia",
+    university: "UIN Syarif Hidayatullah Jakarta",
+    major: "Sastra Inggris",
+    path: "Mandiri",
+    category: "PTN",
+    achievements: ["Wakil Ketua OSIS 2", "Ketua Pelaksana HARTES 2025", "Juara 1 Lomba Band Colabonation"]
+  },
+  {
+    name: "Maesania Vathonah",
+    university: "UPN Veteran Jakarta",
+    major: "Ekonomi Syariah",
+    path: "SNBP",
+    category: "PTN",
+    achievements: []
+  },
+  {
+    name: "Nayla Safira Aritoa",
+    university: "Universitas Diponegoro",
+    major: "Kimia",
+    path: "Mandiri",
+    category: "PTN",
+    achievements: ["Anggota MPK", "Lulus UM Undip (Skor UTBK+Rapot)"]
+  },
+  {
+    name: "Rahma Arifa Wulandari",
+    university: "Institut Teknologi Bandung",
+    major: "FITB",
+    path: "SNBT",
+    category: "PTN",
+    achievements: ["Sekretaris Hang Tuah Cup 2025", "Sekretaris MPK Periode 2024/2025"]
+  },
+  {
+    name: "Neza Naysila Azahra",
+    university: "Institut Pertanian Bogor",
+    major: "Manajemen Sumberdaya Perairan",
+    path: "SNBP",
+    category: "PTN",
+    achievements: ["Peserta OSN Kimia"]
+  },
+  {
+    name: "Alifah Ayra Fauziah",
+    university: "Universitas Negeri Jakarta",
+    major: "Pendidikan Vokasional Konstruksi Bangunan",
+    path: "Mandiri",
+    category: "PTN",
+    achievements: ["Ketua Sekbid Sastra dan Budaya OSIS", "Ekskul Teater"]
+  },
+  {
+    name: "Iqbal Hilal Al Sadam",
+    university: "UPN Veteran Jakarta",
+    major: "Hukum",
+    path: "Mandiri",
+    category: "PTN",
+    achievements: ["Ketua Divisi Acara HT Art Festival 2024", "Anggota OSIS"]
+  },
+  {
+    name: "Muhammad Alief",
+    university: "UIN Syarif Hidayatullah Jakarta",
+    major: "Ilmu Politik",
+    path: "SNBT",
+    category: "PTN",
+    achievements: []
+  },
+  {
+    name: "Hapsari Puspa Wulandari",
+    university: "Universitas Diponegoro",
+    major: "Bisnis Digital",
+    path: "Mandiri",
+    category: "PTN",
+    achievements: ["Ticketing HT Cup 25", "Pengurus English Club"]
+  },
+  {
+    name: "Rifa Antika Zahra",
+    university: "Universitas Diponegoro",
+    major: "Informasi dan Humas",
+    path: "Mandiri",
+    category: "PTN",
+    achievements: ["Ekskul PMR"]
+  },
+  {
+    name: "Roudatul Jannah",
+    university: "UIN Sunan Ampel Surabaya",
+    major: "Psikologi",
+    path: "SNBT",
+    category: "PTN",
+    achievements: ["Anggota MPK"]
+  },
+  {
+    name: "Bunga Aisyah Oktasyawaley",
+    university: "Universitas Indonesia",
+    major: "Teknik Bioproses",
+    path: "Jalur Prestasi (SJP)",
+    category: "PTN",
+    achievements: ["PJ Sponsor Hang Tuah Cup 2025", "Taekwondo"]
+  },
+  {
+    name: "Sevi Villiani",
+    university: "Universitas Brawijaya",
+    major: "Ilmu Politik",
+    path: "SNBP",
+    category: "PTN",
+    achievements: []
+  },
+  {
+    name: "Muhammad Melvin Afandi",
+    university: "Universitas Brawijaya",
+    major: "Perpajakan",
+    path: "Mandiri",
+    category: "PTN",
+    achievements: ["Juara 1 Tournafest Colabonation", "Bakti Mulya 400"]
+  },
+  {
+    name: "Aulia Puspita Dewi",
+    university: "Poltekkes Kemenkes Jakarta II",
+    major: "Kesehatan Lingkungan",
+    path: "Jalur Prestasi",
+    category: "PTN",
+    achievements: ["Anggota Divisi Keamanan HT Cup 2024", "Humas Ratoh Jaroe Cup 2024"]
+  },
+  {
+    name: "Marsya Putri Nhagita",
+    university: "Universitas Muhammadiyah Jakarta",
+    major: "Ilmu Keperawatan",
+    path: "Reguler TES",
+    category: "PTS",
+    achievements: []
+  },
+  {
+    name: "Raviela Mentari Agza",
+    university: "Poltekkes Kemenkes Jakarta II",
+    major: "Analisis Farmasi dan Makanan",
+    path: "Non-Ikatan Dinas",
+    category: "PTN",
+    achievements: ["Pemangku Adat Pramuka 2024", "Bendahara Event HTRF 2024"]
+  },
+  {
+    name: "Raffi Zakihusaini",
+    university: "Institut Pertanian Bogor",
+    major: "Teknologi Hasil Hutan",
+    path: "Mandiri",
+    category: "PTN",
+    achievements: []
+  },
+  {
+    name: "Alya Eriyanti Agustin",
+    university: "Binus University",
+    major: "Accounting",
+    path: "Reguler TES",
+    category: "PTS",
+    achievements: []
+  },
+  {
+    name: "Sherina Salwa Tahira",
+    university: "Binus University",
+    major: "Accounting",
+    path: "Beasiswa",
+    category: "PTS",
+    achievements: []
+  },
+  {
+    name: "Carmen Aurora Wahyudi",
+    university: "Binus University",
+    major: "International Relations Global Class",
+    path: "Beasiswa",
+    category: "PTS",
+    achievements: ["Gold Medalist English & Language Competition"]
+  },
+  {
+    name: "Audrey Athalia",
+    university: "UPN Veteran Jakarta",
+    major: "Kesehatan Masyarakat",
+    path: "Mandiri",
+    category: "PTN",
+    achievements: ["Humas OSIS", "Humpro HT Cup"]
+  },
+  {
+    name: "Muhammad Farrel Athalla Yamin",
+    university: "Universitas Bakrie",
+    major: "Sistem Informasi",
+    path: "Reguler Tanpa TES",
+    category: "PTS",
+    achievements: []
+  },
+  {
+    name: "Putri Cahaya Mentari",
+    university: "Uhamka",
+    major: "Kesehatan Masyarakat",
+    path: "Reguler Tanpa TES",
+    category: "PTS",
+    achievements: ["Sekretaris 2 OSIS", "Ketua Ekskul HTEC", "Bendahara Buku Tahunan 2025"]
+  },
+  {
+    name: "Mohammad Rizaqi Galihandra Nugroho",
+    university: "Universitas Negeri Jakarta",
+    major: "Pendidikan Teknik Mesin",
+    path: "Mandiri",
+    category: "PTN",
+    achievements: ["Wakil Ketua Ekskul Jurnalistik"]
+  },
+  {
+    name: "Ginaris Pangesti Wibowo",
+    university: "Universitas Brawijaya",
+    major: "Keuangan dan Perbankan",
+    path: "Mandiri",
+    category: "PTN",
+    achievements: []
+  },
+  {
+    name: "Khalysha Radeftia Azzahra",
+    university: "Universitas Negeri Jakarta",
+    major: "Pendidikan Anak Usia Dini",
+    path: "Mandiri",
+    category: "PTN",
+    achievements: ["Juara 1 Olimpiade PKN, Sosiologi, Bahasa Indonesia"]
+  },
+  {
+    name: "Reyhanatul Quddus",
+    university: "Universitas Indonesia",
+    major: "Ilmu Keperawatan",
+    path: "SNBT",
+    category: "PTN",
+    achievements: []
+  },
+  {
+    name: "Caesar Muhammad Zulkarnaen",
+    university: "Universitas Islam Indonesia",
+    major: "Hukum",
+    path: "Reguler Tanpa TES",
+    category: "PTS",
+    achievements: ["Ketua Basket SMA Hang Tuah", "Juara 2 Basket Hukum Nasional Unpad"]
+  }
 ];
 
-export const FORMS_URL = "https://forms.gle/iqpN6XJ24psDcwwf6"; 
+export const FORMS_URL = "https://forms.gle/iqpN6XJ24psDcwwf6";
 export const CONTACT_WA = "https://wa.me/6282114927981";
-export const COMMUNITY_WA_GROUP = "https://chat.whatsapp.com/L4p8G9x2m4B9z8x2m4B9z8"; // Placeholder, update as needed
+export const COMMUNITY_WA_GROUP = "https://chat.whatsapp.com/L4p8G9x2m4B9z8x2m4B9z8";
